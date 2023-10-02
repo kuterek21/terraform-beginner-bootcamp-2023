d3abfb17773c0cdb70cf1733ad7e055c12a9088c
Mariusz  Futerski <mfuterski@yahoo.com>
2023-10-02 15:33:39 +0000

    #11 install terraform provider and create S3 bucket

--- bin/.env.example.sh
+++ bin/.env.example.sh
@@ -1,1 +1,16 @@
 PROJECT_ROOT='/workspace/terraform-beginner-bootcamp-2023'
+
+AWS_ACCESS_KEY_ID=' '
+AWS_SECRET_ACCESS_KEY=' '
+AWS_DEFAULT_REGION=' '
+
+# Do NOT commit those to your REPO !!!
+
+export AWS_ACCESS_KEY_ID=' '
+export AWS_SECRET_ACCESS_KEY=' '
+export AWS_DEFAULT_REGION=' '
+
+gp ebv AWS_ACCESS_KEY_ID=' '
+gp env AWS_SECRET_ACCESS_KEY=' '
+gp env AWS_DEFAULT_REGION=' '
+
