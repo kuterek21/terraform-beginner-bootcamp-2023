8824ed90069e4782789c609b69b6c2eabac2aaa7
Mariusz  Futerski <mfuterski@yahoo.com>
2023-10-07 19:25:32 +0000

    providers updated

--- main.tf
+++ main.tf
@@ -1,22 +1,5 @@
-terraform {
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.19.0"
-  }
-}
-  }
-provider "aws" {
-  # Configuration options
-}
 
-provider "random" {
 
-}
 resource "random_string" "bucket_name" {
     lower = true#
     upper = false
---
+++ providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.19.0"
+  }
+}
+  }
+provider "aws" {
+  # Configuration options
+}
+
+provider "random" {
+
+}
